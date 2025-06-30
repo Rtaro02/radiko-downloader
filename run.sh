c0bebb4a6253a2978b5e02f068ef1d02297ac22e
rtraro02 <nmeeaiwxntwsoetnlelin@gmail.com>
2025-06-30 12:33:24 +0900

    feat: radiko-downloaderのCronJobとスクリプトを更新
    - radiko-downloader-cronjob.yaml:
      - コンテナ名を「radiko-downloader」から「as1422-radiko-downloader」に変更
      - 環境変数PROGRAM_START_TIMEを追加し、値を"00:00"に設定
    
    - run.sh:
      - DATEの設定ロジックを修正し、PROGRAM_START_TIMEを使用するように変更

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-if [ "$(date +%u)" -eq 1 ]; then
-  # 今日が月曜（曜日=1）なら、今日の00:00
-  DATE=$(date -d "00:00" +"%Y%m%d%H%M%S")
-else
-  # それ以外は直近の月曜00:00
-  DATE=$(date -d "last monday 00:00" +"%Y%m%d%H%M%S")
-fi
+DATE=$(date -d ${PROGRAM_START_TIME} +"%Y%m%d%H%M%S")
 
 echo "Recording date: ${DATE}"
 ./rec_radiko_ts.sh -u https://radiko.jp/#!/ts/JORF/${DATE} -o as1422_${DATE}
